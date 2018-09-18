9cad01d55fbf9b6df28dca890bc4f65aee3b80a4
Alexey Mavrin <amavrin@gmail.com>
2018-09-19 02:35:52 +0300

    add new parameters - verbose and matrix_cmd add exception

--- access_matrix.py
+++ access_matrix.py
@@ -32,8 +32,6 @@ def rights2str(s):
     xstr = ""
     if len(s) == 0:
         xstr = u"Запрет"
-    elif 'full' in s:
-        xstr = u"Полные права"
     else:
         xstr = ", ".join(map( verbose_x, s))
 
@@ -50,6 +48,13 @@ def read_config(conffile):
     Cfg['resno'] = int( config.get('config','number_of_resources') )
     Cfg['users'] = config.get('config','users').split()
     Cfg['admin'] = config.get('config','admin')
+
+    if config.get('config','verbose') == "on":
+        Cfg['verbose'] = True
+    else:
+        Cfg['verbose'] = False
+
+    Cfg['matrix_cmd'] = config.get('config','matrix_cmd')
 
 # вывод конфигурационных параметров
 def print_config():
@@ -81,50 +86,85 @@ def gen_matrix():
         Matrix[u] = {}
         for r in range(1, Cfg['resno'] + 1):
             if u == Cfg['admin']:
-                s = Set(['full'])
+                s = Set(['read', 'write', 'grant'])
                 Matrix[u][r] = s
             else:
                 s = get_random_set()
                 Matrix[u][r] = s
             #print "*** " + u + " " + obj_num2name(r) + ": " + " ".join(Matrix[u][r])
 
-def check_acess(u, r, c):
-    if c in Matrix[u][r] or 'full' in Matrix[u][r]:
+def get_resource_num(message):
+    sys.stdout.write(message)
+    r = raw_input(" ")
+    if r == "":
+        print u"Объект не выбран"
         return True
-    else:
-        return False
 
-def process_user_command(u):
-    sys.stdout.write(u"Жду ваших указаний")
-    c = raw_input(" > ")
-    c = c.rstrip()
-    if c == 'read' or c == 'write':
-        sys.stdout.write(u"Над каким объектом производится операция")
-        r = raw_input("? ")
-        if r == "":
-            print u"Объект не выбран"
-            return True
+    try:
         r = int(r)
-        if check_acess(u, r, c):
-            print u"Операция прошла успешно"
-        else:
-            print u"Отказ в выполнении операции. У Вас нет прав для ее осуществления"
+    except:
+        print u"введите целое число"
+        return 0
+
+    return r
+
+def process_grant(u, r):
+    r = get_resource_num(u"Право на какой объект передается?")
+    if r == 0:
+        raise Exception(u"не получен номер объекта для передачи прав")
+
+def check_access(u, r, c):
+    if r not in range(1,Cfg['resno'] + 1):
+        raise Exception(u"ресурс не определён, введите число от 1 до " + str(Cfg['resno']))
+
+    if c in Matrix[u][r]:
         return True
-    elif c == 'grant':
-        print "XXX not implemented"
-        return True
-    elif c == "":
-        return True
-    elif c == 'quit':
-        return False
     else:
-        print u"Задана недопустимая операция"
-        return True
+        return False
 
 def print_matrix():
     for u in (Cfg['users']):
         for r in (Matrix[u].keys()):
             print u"Права " + u + u" на ресурс " + obj_num2name(r) + u": " + " ".join(Matrix[u][r])
+
+def process_user_command(u):
+    sys.stdout.write(u"Жду ваших указаний")
+
+    r = 0
+
+    try:
+        c = raw_input(" > ")
+    except:
+        # пользователь нажал control-D, выход из системы
+        sys.stdout.write("\n")
+        c = "quit"
+
+    c = c.rstrip()
+    if c == 'read' or c == 'write':
+        r = get_resource_num(u"Над каким объектом производится операция?")
+
+    elif c == "":
+        return True
+    elif c == 'quit':
+        print u"Работа пользователя " + u + u" завершена. До свидания."
+        return False
+    elif c == 'matrix' and Cfg['matrix_cmd'] == "on":
+        print_matrix()
+        return True
+    else:
+        print u"Задана недопустимая операция"
+        return True
+
+    try:
+        if check_access(u, r, c):
+            print u"Операция прошла успешно"
+        else:
+            print u"Отказ в выполнении операции. У Вас нет прав для ее осуществления"
+    except Exception as e:
+        m, = e.args
+        sys.stdout.write(u" *** ошибка проверки прав доступа: ")
+        print m.encode("utf-8")
+    return True
 
 def print_user_rights(u):
     print u"Перечень Ваших прав:"
@@ -133,7 +173,7 @@ def print_user_rights(u):
         print rights2str(Matrix[u][r])
 
 def user_login():
-    name = raw_input("User:")
+    name = raw_input("User: ")
     if name == "":
         return
     if name not in Cfg['users']:
@@ -149,14 +189,18 @@ def user_login():
 parser = argparse.ArgumentParser(description=u'Матрица доступа к ресурсам (дискреционная модель)')
 parser.add_argument('-c', dest='conffile', default="access_matrix.ini", help=u'конфигурационный файл')
 args = parser.parse_args()
+
 read_config(args.conffile)
-print_config()
+if Cfg['verbose']:
+    print_config()
+
 gen_matrix()
-print_matrix()
+if Cfg['verbose']:
+    print_matrix()
 
 while 1:
     try:
         user_login()
     except EOFError:
-        print u"\nДо свидания"
+        print u"\nРабота системы завершена. До свидания"
         exit(0)
